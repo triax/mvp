03fc57588eb18c61b6ca42e39ce2bdfdef8d7762
Hiromu OCHIAI <otiai10@gmail.com>
2023-06-03 16:52:50 +0900

    Thank you

--- src/containers/Ranking.tsx
+++ src/containers/Ranking.tsx
@@ -4,9 +4,10 @@ import TeamSwitchView from "../components/TeamSwitch";
 import { votesToEntries } from "../models/RankingEntry";
 import PlayerRankingItem from "../components/PlayerRankingItem";
 import { useLoaderData, useNavigate, type LoaderFunction } from "react-router-dom";
-import { default as Game } from "../models/common/Game";
+import { default as Game, Status } from "../models/common/Game";
 import { default as Vote } from "../models/common/Vote";
 import { cooltimeRoutine, humanize } from "../utils";
+import GameTitle from "../components/GameTitle";
 
 export const loader: LoaderFunction = async ({ params }) => {
     const game = await Game.get(params.gameId!);
@@ -28,19 +29,33 @@ export default function RankingView() {
 
     const entries = votesToEntries(game, votes, (vote) => vote.side === side);
 
+
+    const status = game.getStatus();
+
+    const getActionableSection = () => {
+        if (status == Status.CLOSED || status == Status.FINISHED) return <div>
+        </div>;
+        if (myself.canVote()) return <button style={{ width: "100%" }}>
+            投票する
+        </button>;
+        return <button style={{ width: "100%" }} disabled>
+            {humanize(-1 * cooltime)}後に投票可能
+        </button>;
+    }
+
+
+
     return (
         <div>
-            <h2>現在の投票順位</h2>
+            {(status == Status.FINISHED || status == Status.CLOSED) ? <>
+                <h2>ご声援ありがとうございました！</h2>
+                <GameTitle game={game} />
+                <h2 style={{textAlign: "center"}}>~~ 最終結果 ~~</h2>
+            </> : <h2>現在の投票順位</h2>}
             <TeamSwitchView game={game} side={side} switchSide={switchSide} />
             <div style={{margin: "16px 0"}}
                 onClick={() => navigate(`/_g/${game.id}`)}
-            >
-                {myself.canVote() ? <button style={{ width: "100%" }}>
-                    投票する
-                </button> : <button style={{ width: "100%" }} disabled>
-                    {humanize(-1 * cooltime)}後に投票可能
-                </button>}
-            </div>
+            >{getActionableSection()}</div>
             {entries.map((entry, i) => <PlayerRankingItem
                 rank={i + 1}
                 key={entry.member.id} entry={entry}
--- src/containers/Vote.tsx
+++ src/containers/Vote.tsx
@@ -4,9 +4,9 @@ import { useEffect, useState } from "react";
 import PlayerItem from "../components/PlayerItem";
 
 
-import { default as NeuGame } from "../models/common/Game";
+import { default as NeuGame, Status } from "../models/common/Game";
 import { default as NeuMember } from "../models/common/Member";
-import { default as NeuVote } from "../models/common/Vote";
+// import { default as NeuVote } from "../models/common/Vote";
 import { useLoaderData, useNavigate, type LoaderFunction } from "react-router-dom";
 import { cooltimeRoutine, shuffle } from "../utils";
 
@@ -31,28 +31,49 @@ export default function VoteView() {
     const [query, setQuery] = useState<string>(url.searchParams.get("q") || "");
     const [side, setSide] = useState<string>(location.hash.replace("#", "") || "visitor");
 
-     const upvote = async (member: NeuMember) => {
-        const vote = new NeuVote({
-            game_id: game.id!, game,
-            member_id: member.id!, member,
-            side: side as "visitor"| "home",
-            timestamp: Date.now(),
-            uuid: myself.uuid,
-        });
-        await vote.insert();
-        await myself.update({
-            voted: { ...myself.voted, [game.id!]: true },
-            lastVotedTimestamp: vote.timestamp,
-        });
-        setCooltime(myself.secondsUntilRevote());
-        navigate(`/_g/${game.id}/_v`);
-    };
+    //  const upvote = async (member: NeuMember) => {
+    //     const vote = new NeuVote({
+    //         game_id: game.id!, game,
+    //         member_id: member.id!, member,
+    //         side: side as "visitor"| "home",
+    //         timestamp: Date.now(),
+    //         uuid: myself.uuid,
+    //     });
+    //     await vote.insert();
+    //     await myself.update({
+    //         voted: { ...myself.voted, [game.id!]: true },
+    //         lastVotedTimestamp: vote.timestamp,
+    //     });
+    //     setCooltime(myself.secondsUntilRevote());
+    //     navigate(`/_g/${game.id}/_v`);
+    // };
 
     const filter = (p: NeuMember) => {
         if (query === "") return true;
         return [p.name, p.name_eng, p.name_yomi, p.number, p.position].some((s = "") => s.includes(query));
     }
     const _s = side == "home" ? "home" : "visitor";
+
+    const getActionableSection = () => {
+        const s = game.getStatus();
+        if (s == Status.CLOSED || s == Status.FINISHED) return <div style={{ display: "flex" }}>
+            <button
+                style={{flex: 1}}
+                onClick={() => navigate(`/_g/${game.id}/_v`)}
+            >結果を見る</button>
+        </div>;
+        if (cooltime < 0) return <div style={{ display: "flex" }}>
+            <span>再投票まであと{-1 * cooltime}秒</span>
+        </div>;
+        return <div style={{ display: "flex" }}>
+            <input type="text"
+                style={{ flex: 1, fontSize: "0.8rem", height: "2rem", border: "1px solid #d0d0d0", paddingLeft: "0.4rem" }}
+                placeholder="🔍 名前、背番号、ふりがな、ポジション"
+                onChange={(e) => setQuery(e.target.value)}
+                defaultValue={query}
+            />
+        </div>;
+    }
 
     return (
         <div>
@@ -62,21 +83,13 @@ export default function VoteView() {
                 switchSide={(side) => setSide(side)}
             />
             <div>
-                {cooltime < 0 ? <div style={{ display: "flex" }}>
-                    <span>再投票まであと{-1 * cooltime}秒</span>
-                </div> : <div style={{ display: "flex" }}>
-                    <input type="text"
-                        style={{ flex: 1, fontSize: "0.8rem", height: "2rem", border: "1px solid #d0d0d0", paddingLeft: "0.4rem"}}
-                        placeholder="🔍 名前、背番号、ふりがな、ポジション"
-                        onChange={(e) => setQuery(e.target.value)}
-                        defaultValue={query}
-                    />
-                </div>}
+                {getActionableSection()}
                 {shuffle(players[_s]).filter(filter).map((player) => <PlayerItem
                     key={player.id}
                     player={player}
                     defaultIcon={game[_s].icon_image_url}
-                    upvote={upvote}
+                    // TODO: Dynamicにする
+                    // upvote={ upvote}
                     myself={myself}
                 />)}
             </div>
