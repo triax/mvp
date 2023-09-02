0cbbe83851a704bd09fa7ebde002512ea25635c8
Hiromu OCHIAI <otiai10@gmail.com>
2023-09-02 11:49:00 +0900

    Aboutを中央寄せにした

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,21 +11,23 @@ export default function AboutView({
 }) {
     return (
         <>
-            {divider ? <div style={{ textAlign: "center", marginTop: "18px" }}>***</div> : null}
-            <h3>リアルタイム投票システムとは？</h3>
-            <span>「リアルタイム投票システム」は、観客の皆様と試合およびチームの距離をもっと近づけ、アメリカンフットボールの試合をより双方向で楽しめるコンテンツにすべく、<b>CLUB TRIAX に所属する職業プログラマのメンバーにより独自に開発された</b>システムです。</span>
-            <div>より詳しい説明は、<a href="https://www.triax.football/about/mvp-system" target="_blank">こちら</a>をご参照ください。<a href="https://www.triax.football/about/mvp-system" target="_blank">https://www.triax.football/about/mvp-system</a></div>
-            {social ? <div style={{marginTop: "18px"}}>
-                <a href="https://www.instagram.com/clubtriax/" target="_blank">
-                    <img src={instagramIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
-                </a>
-                <a href="https://www.tiktok.com/@clubtriax" target="_blank">
-                    <img src={tiktokIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
-                </a>
-                <a href="https://twitter.com/clubtriax" target="_blank">
-                    <img src={twitterIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
-                </a>
-            </div> : null}
+            <div style={{textAlign: "center"}}>
+                {divider ? <div style={{ textAlign: "center", marginTop: "18px" }}>***</div> : null}
+                <h3>リアルタイム投票システムとは？</h3>
+                <span>「リアルタイム投票システム」は、観客の皆様と試合およびチームの距離をもっと近づけ、アメリカンフットボールの試合をより双方向で楽しめるコンテンツにすべく、<b>CLUB TRIAX に所属する職業プログラマのメンバーにより独自に開発された</b>システムです。</span>
+                <div>より詳しい説明は、<a href="https://www.triax.football/about/mvp-system" target="_blank">こちら</a>をご参照ください。<a href="https://www.triax.football/about/mvp-system" target="_blank">https://www.triax.football/about/mvp-system</a></div>
+                {social ? <div style={{marginTop: "18px"}}>
+                    <a href="https://www.instagram.com/clubtriax/" target="_blank">
+                        <img src={instagramIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
+                    </a>
+                    <a href="https://www.tiktok.com/@clubtriax" target="_blank">
+                        <img src={tiktokIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
+                    </a>
+                    <a href="https://twitter.com/clubtriax" target="_blank">
+                        <img src={twitterIcon} style={{ height: "1.8rem", marginRight: "0.5rem" }} />
+                    </a>
+                </div> : null}
+            </div>
         </>
     )
 }
